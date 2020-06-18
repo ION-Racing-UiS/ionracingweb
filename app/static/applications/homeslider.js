05bdef4ac3d0aafb5bcd745c38d9cfece2cd6a3d
Yohannes Kassaye <y.kassaye@ionracing.no>
2020-06-18 23:27:59 +0200

    Fiks so the slider works with mouseevent

--- app/static/applications/homeslider.js
+++ app/static/applications/homeslider.js
@@ -11,21 +11,49 @@ function currentSlide(n) {
 function showslide(n) {
     var i;
     var slides = document.getElementsByClassName("carslides");
-    var dots = document.getElementsByClassName("dot");
+    // var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1 };
     if (n < 1) {slideIndex = slides.length};
     for(i = 0; i < slides.length; i++){
         console.log(slides[i].style.display);
         slides[i].style.display = "none";
     }
-    
-    for(i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+    // for(i = 0; i < dots.length; i++){
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
     slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className += "active";
+    // dots[slideIndex - 1].className += "active";
 }
-
+var ele_rect;
 document.addEventListener("DOMContentLoaded", () =>{
     showslide(slideIndex);
+    var ele = $(".carContainer")[0];
+    ele_rect = ele.getBoundingClientRect();
+    var interval = 500;
+    var i;
+
+    $(document).mousemove(function(e){
+        if (e.pageX > ele_rect.x && e.pageX < (ele_rect.right - (ele_rect.x / ele_rect.left)) 
+            && e.pageY > ele_rect.y && e.pageY < ele_rect.bottom) {
+            
+            if (e.pageX > ele_rect.right - ele_rect.width / 4) {
+                ele.style.background = "blue";
+                i = setInterval(function () {
+                    console.log("hei");
+                    
+                    slidechange(1);
+                }, interval);
+                
+            } else if (e.pageX < ele_rect.x + ele_rect.width / 4) {
+                ele.style.background = "red";
+                
+                i = setInterval(function () {
+                    console.log("hei2");
+                    slidechange(-1);
+                }, interval);
+            }
+            clearInterval(i);
+        }
+        // setTimeout(this, 1000);
+    });
 })
