a71913f80bad7baaf94dacaf2aa9183c7513ad17
unknown <Administrator@DEV.IONRACING.NO>
2020-08-23 02:21:31 +0200

    Added missing files from previous commit.

---
+++ schema.sql
@@ -0,0 +1,28 @@
+create database if not exists ionracing;
+
+use ionracing;
+
+create table if not exists car (
+	`id` integer not null auto_increment /* int for id of car, auto_incremented */,
+    `year` integer not null /* year for the car */,
+    `name` varchar(48) /* name of the car */,
+    `number` integer /* racenumber of the car */,
+    `img` text /* path for the picture of the car */,
+    `mass` float /* mass of the car in kg */,
+    `engine` varchar(64) /* type of the engine in the car */,
+    `output` varchar(16) /* output of the car in kW and/or PS */,
+    `torque` float /* torque of the car in Nm */,
+    primary key(`id`)
+);
+
+create table if not exists post (
+	`pid` integer not null auto_increment /* int for id of the post */,
+    `author` varchar(96) not null /* author of the post */,
+    `title` varchar(48) not null /* title of the post */,
+    `datetime` datetime /* date and time of the post in the format YYYY-MM-DD hh:mm:ss */,
+    `heading` text /* heading for the post */,
+    `text` text /* main text of the post */,
+    `bgimg` text /* path to the background image */,
+    `img` text /* path to the main image */,
+    primary key(`pid`, `author`, `title`, `datetime`)
+);
